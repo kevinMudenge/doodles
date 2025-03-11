df5308c5356606dc858ef2bae0cd80e418958a17
kevin Mudenge <mudengekevn254@gmail.com>
2025-03-11 06:37:56 +0300

    hpw annoying

---
+++ testimonialGenerator/script.js
@@ -0,0 +1,28 @@
+const imageEl = document.querySelector('img');
+const testimonial = document.querySelector('.testimonial');
+const h4El= document.querySelector('h4');
+
+const testimonies = [
+    {
+        imageEl:"",
+        testimonial: "I am a Testimony",
+        h4El:"kevin Mudenge",
+    },
+    {
+        imageEl:"",
+        testimonial: "I am a Testing",
+        h4El:"jack Olson",
+    },
+    {
+        imageEl:"",
+        testimonial: "We will generate them all.",
+        h4El:"software Company",
+    },
+]
+
+updateTestimonies();
+
+function updateTestimonies(){
+    let testimonyIndex = 0;
+    
+}
